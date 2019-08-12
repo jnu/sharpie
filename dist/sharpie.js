1002f7ba77d4802b63e9a39903dae7324236c5bd
Joe Nudell <jnu@stanford.edu>
2019-08-12 15:03:51 -0400

    rebuild

--- dist/sharpie.js
+++ dist/sharpie.js
@@ -222,17 +222,42 @@ function getSharpieExtent(range) {
 
 function selectDelegate(e) {
   var selection = window.getSelection();
+  var selectCallbacks = resolveHandlersForSelection("select", selection);
 
-  if (selection.isCollapsed) {
-    util_1._debug("Ignoring collapsed selection");
+  if (!selectCallbacks) {
+    util_1._debug("Ignoring selection due to overflow");
 
     return;
   }
 
-  var callbacks = resolveHandlersForSelection("select", selection);
+  var deselectCallbacks = resolveHandlersForSelection("deselect", selection);
 
-  if (!callbacks) {
-    util_1._debug("Ignoring selection due to overflow");
+  if (selection.isCollapsed) {
+    util_1._debug("Firing deselect callbacks");
+
+    var _iteratorNormalCompletion2 = true;
+    var _didIteratorError2 = false;
+    var _iteratorError2 = undefined;
+
+    try {
+      for (var _iterator2 = (deselectCallbacks || [])[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+        var cb = _step2.value;
+        cb(e);
+      }
+    } catch (err) {
+      _didIteratorError2 = true;
+      _iteratorError2 = err;
+    } finally {
+      try {
+        if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
+          _iterator2["return"]();
+        }
+      } finally {
+        if (_didIteratorError2) {
+          throw _iteratorError2;
+        }
+      }
+    }
 
     return;
   }
@@ -247,26 +272,27 @@ function selectDelegate(e) {
     }
 
     var extent = getSharpieExtent(range);
-    var _iteratorNormalCompletion2 = true;
-    var _didIteratorError2 = false;
-    var _iteratorError2 = undefined;
+    var _iteratorNormalCompletion3 = true;
+    var _didIteratorError3 = false;
+    var _iteratorError3 = undefined;
 
     try {
-      for (var _iterator2 = callbacks[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-        var cb = _step2.value;
-        cb(extent, e);
+      for (var _iterator3 = selectCallbacks[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+        var _cb = _step3.value;
+
+        _cb(extent, e);
       }
     } catch (err) {
-      _didIteratorError2 = true;
-      _iteratorError2 = err;
+      _didIteratorError3 = true;
+      _iteratorError3 = err;
     } finally {
       try {
-        if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
-          _iterator2["return"]();
+        if (!_iteratorNormalCompletion3 && _iterator3["return"] != null) {
+          _iterator3["return"]();
         }
       } finally {
-        if (_didIteratorError2) {
-          throw _iteratorError2;
+        if (_didIteratorError3) {
+          throw _iteratorError3;
         }
       }
     }
@@ -302,26 +328,31 @@ function createHoverDelegate(eventType) {
     }
 
     var callbacks = getWatchHandlers(eventType, target);
-    var _iteratorNormalCompletion3 = true;
-    var _didIteratorError3 = false;
-    var _iteratorError3 = undefined;
+
+    if (!callbacks) {
+      return;
+    }
+
+    var _iteratorNormalCompletion4 = true;
+    var _didIteratorError4 = false;
+    var _iteratorError4 = undefined;
 
     try {
-      for (var _iterator3 = callbacks[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-        var cb = _step3.value;
+      for (var _iterator4 = callbacks[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+        var cb = _step4.value;
         cb(parsedId, e);
       }
     } catch (err) {
-      _didIteratorError3 = true;
-      _iteratorError3 = err;
+      _didIteratorError4 = true;
+      _iteratorError4 = err;
     } finally {
       try {
-        if (!_iteratorNormalCompletion3 && _iterator3["return"] != null) {
-          _iterator3["return"]();
+        if (!_iteratorNormalCompletion4 && _iterator4["return"] != null) {
+          _iterator4["return"]();
         }
       } finally {
-        if (_didIteratorError3) {
-          throw _iteratorError3;
+        if (_didIteratorError4) {
+          throw _iteratorError4;
         }
       }
     }
@@ -370,6 +401,10 @@ function watch(element) {
     },
     select: function select(handler) {
       getEventHandlers(registry, "select").add(handler);
+      return ctl;
+    },
+    deselect: function deselect(handler) {
+      getEventHandlers(registry, "deselect").add(handler);
       return ctl;
     }
   };
--- lib/highlight.d.ts
+++ lib/highlight.d.ts
@@ -1,8 +1,9 @@
-export declare type SharpieEvent = "select" | "hoverIn" | "hoverOut";
+export declare type SharpieEvent = "deselect" | "select" | "hoverIn" | "hoverOut";
 export declare function watch(element: HTMLElement): {
     hoverIn: (handler: Function) => any;
     hoverOut: (handler: Function) => any;
     select: (handler: Function) => any;
+    deselect: (handler: Function) => any;
 };
 export declare function unwatch(element: HTMLElement, eventType?: SharpieEvent, handler?: Function): boolean;
 export declare function clearSelection(): void;
